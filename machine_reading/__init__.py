bd56590bb2d68814036f39c26e06140ea20d633d
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2022-12-16 12:09:52 +0100

    Update in __init__.py

--- machine_reading/__init__.py
+++ machine_reading/__init__.py
@@ -2,4 +2,3 @@ from machine_reading.api.core import *
 from machine_reading.core.workflows import *
 from machine_reading.readers.fred import *
 from machine_reading.readers.amr import *
-from machine_reading.mr import *
